28820c5e3bd497cc957ddeae87f223b90a75a531
lutogb27 <lutogb27@yahoo.co.jp>
2023-06-10 15:41:33 +0900

    バリデーションの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   validates :start_at, presence: true
   validates :end_at, presence: true
   validates :memo, length: { maximum: 500 } 
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:end_at, "は開始日より前の日付は登録できません。") unless
+    self.start_at < self.end_at 
+    end
 end
